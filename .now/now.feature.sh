8657ea945c84a222a69156aaae4c8f24df882e48
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2018-12-30 20:33:18 +0000

    now-deployment

---
+++ .now/now.feature.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+CLEAN_BRANCH_NAME=${CIRCLE_BRANCH//\//-};
+
+JSON=$(cat <<-EOF
+{
+    "name": "$CIRCLE_PROJECT_REPONAME-$CLEAN_BRANCH_NAME",
+    "alias": "$CLEAN_BRANCH_NAME-circleci.now.sh",
+    "builds": [
+        {
+            "src": "package.json",
+            "use": "@now/static-build"
+        }
+    ],
+    "routes": [
+        {
+            "src": "^/static/(.*)",
+            "dest": "/static/$1"
+        },
+        {
+            "src": "^/favicon.ico",
+            "dest": "/favicon.ico"
+        },
+        {
+            "src": "^/asset-manifest.json",
+            "dest": "/asset-manifest.json"
+        },
+        {
+            "src": "^/manifest.json",
+            "dest": "/manifest.json"
+        },
+        {
+            "src": "^/service-worker.js",
+            "headers": {
+                "cache-control": "s-maxage=0"
+            },
+            "dest": "/service-worker.js"
+        },
+        {
+            "src": "^/precache-manifest.(.*)",
+            "dest": "/precache-manifest.$1"
+        },
+        {
+            "src": "^/(.*)",
+            "dest": "/index.html"
+        }
+    ]
+}
+EOF
+)
+
+echo $JSON > .now/now.feature.json

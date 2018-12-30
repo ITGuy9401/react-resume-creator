74e9fa79444755d049e0b647265852f04080f14d
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2018-12-30 18:47:48 +0000

    de

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Form from "./views/Form";
 import Result from "./views/Result";
+import "bulma/bulma.sass";
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <Route exact path="/" component={Form}/>
-                <Route path="/result" component={Result}/>
+                <div>
+                    <Route exact path="/" component={Form}/>
+                    <Route path="/result" component={Result}/>
+                </div>
             </Router>
         );
     }
--- src/views/Form.js
+++ src/views/Form.js
@@ -1,11 +1,72 @@
 import React from "react";
 import {withRouter} from "react-router-dom";
+import {Container, Content, Control, Field, Input, Label, Select, Subtitle, Title} from "bloomer";
 
 class Form extends React.Component {
 
+    state = {
+        form: {}
+    };
+
+    constructor(props) {
+        super(props);
+        this.onInputUpdate = this.onInputUpdate.bind(this);
+    }
+
+    onInputUpdate(event) {
+        const form = {...this.state.form};
+        form[event.target.name] = event.target.value;
+        this.setState({form: form});
+    }
+
     render() {
         return (
-            <h1>Hello World</h1>
+            <Container>
+                <Title>Resume Creator</Title>
+                <Subtitle>Fill the form to create your resume</Subtitle>
+                <Content>
+                    <form className="form">
+                        <Field>
+                            <Label>First name</Label>
+                            <Control>
+                                <Input type="text" placeholder='Es. Anna' name="first_name"
+                                       onChange={this.onInputUpdate}/>
+                            </Control>
+                        </Field>
+                        <Field>
+                            <Label>Last name</Label>
+                            <Control>
+                                <Input type="text" placeholder='Es. Wright' name="last_name"
+                                       onChange={this.onInputUpdate}/>
+                            </Control>
+                        </Field>
+                        <Field>
+                            <Label>Gender</Label>
+                            <Control>
+                                <Select>
+                                    <option value="M">Male</option>
+                                    <option value="F">Female</option>
+                                    <option value="X">Other</option>
+                                </Select>
+                            </Control>
+                        </Field>
+                        <Field>
+                            <Label>E-Mail</Label>
+                            <Control>
+                                <Input type="email" placeholder='Es. annawright@gmail.com' name="email"
+                                       onChange={this.onInputUpdate}/>
+                            </Control>
+                        </Field>
+                        <Field>
+                            <Label>Phone</Label>
+                            <Control>
+                                <Input type="text" placeholder='Es. +44 (0) 7777 777777' name="phone"
+                                       onChange={this.onInputUpdate}/>
+                            </Control>
+                        </Field>
+                    </form>
+                </Content>
+            </Container>
         );
     }
 }

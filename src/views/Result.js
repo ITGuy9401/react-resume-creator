cc6b4091fc95ada0d1efd293de6e6da51f05bbd4
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2018-12-31 00:00:55 +0000

    config and first development

--- src/views/Result.js
+++ src/views/Result.js
@@ -1,12 +1,33 @@
 import React from "react";
 import {withRouter} from "react-router-dom";
 import {withUserInformationCtx} from "../context/UserInformationContext";
+import {Content, Hero, HeroBody, Subtitle, Title} from "bloomer";
 
 class Result extends React.Component {
 
     render() {
+        const {userInfo} = this.props;
+        const {form} = userInfo;
         return (
-            <h1>Hello Result</h1>
+            <div>
+                <Hero>
+                    <HeroBody>
+                        <Content>
+                            <Title className="has-text-centered">{form.first_name} {form.last_name}</Title>
+                            <p>
+                                {form.address} | {form.email} | {form.phone}
+                            </p>
+                        </Content>
+                    </HeroBody>
+                </Hero>
+                <Subtitle>Brief Summary</Subtitle>
+                <div>
+                    {form.biography}
+                </div>
+                <br/>
+                <Subtitle>Work Experience</Subtitle>
+
+            </div>
         );
     }
 }
